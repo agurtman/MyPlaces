5344d3565b6f468d6721a72235237d6d6673b1be
agurtman <104502852+agurtman@users.noreply.github.com>
2024-01-25 17:51:32 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Артём Гуртман on 25.01.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
