6c61b3630a7ede2947926d998db5df7a4f8c69be
agurtman <104502852+agurtman@users.noreply.github.com>
2024-01-30 15:58:22 +0300

    Design for NewPlaceViewController

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,39 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Артём Гуртман on 30.01.2024.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+    
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
